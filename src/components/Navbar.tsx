d80295849320d845cb18c2a79fab4b2297cac1a6
James-Ian09 <jamesianbayonas21@gmail.com>
2025-05-01 00:19:23 +0800

    Made some change's

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { appleImg } from "../utils";
+import { appleImg, searchImg } from "../utils";
 
 const Navbar = () => {
   return (
@@ -6,6 +6,16 @@ const Navbar = () => {
       <nav>
         <img src={appleImg} alt="Apple" width={14} height={18} />
       </nav>
+
+      <div>
+        {["Legion", "Think Pad", "Yoga-Series", "Idea-Pad"].map((nav) => (
+          <div key={nav}>{nav}</div>
+        ))}
+      </div>
+
+      <div>
+        <img src={searchImg} alt="search" width={18} height={18} />
+      </div>
     </header>
   );
 };
