c692d28657fbb8e5bd93ce83afb096441f2e7793
James-Ian09 <jamesianbayonas21@gmail.com>
2025-05-06 11:43:02 +0800

    Added some desings

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,17 +3,16 @@ import { appleImg, searchImg, bagImg } from "../utils";
 const Navbar = () => {
   // const is just a convention, you can use let or var as well
   return (
-    <header>
+    <header className="w-full py-5 sm:px-10 px-5 flex-justify-between items-center">
       <nav>
         <img src={appleImg} alt="Apple" width={14} height={18} />
       </nav>
-
+      "
       <div>
         {["Legion", "Think Pad", "Yoga-Series", "Idea-Pad"].map((nav) => (
           <div key={nav}>{nav}</div>
         ))}
       </div>
-
       <div>
         <img src={searchImg} alt="search" width={18} height={18} />
         <img src={bagImg} alt="" width={18} height={18} />
