4d8aec690d27b4b5ecbbc56d6a5cddbe4231502a
James-Ian09 <jamesianbayonas21@gmail.com>
2025-05-06 12:33:19 +0800

    I have added some code functions

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,23 @@
 import { appleImg, searchImg, bagImg } from "../utils";
+import { navList } from "../constants/constants.d.ts";
 
 const Navbar = () => {
   // const is just a convention, you can use let or var as well
   return (
-    <header className="w-full py-5 sm:px-10 px-5 flex-justify-between items-center">
-      <nav>
+    <header className="w-full py-5 sm:px-10 px-5 flex-justify-">
+      <nav className="flex w-full screen-max-width">
         <img src={appleImg} alt="Apple" width={14} height={18} />
+
+        <div className="flex flex-1 justify-center max-sm:hidden">
+          {navList.map((nav: string) => (
+            <div key={nav}>{nav}</div>
+          ))}
+        </div>
+        <div>
+          <img src={searchImg} alt="search" width={18} height={18} />
+          <img src={bagImg} alt="" width={18} height={18} />
+        </div>
       </nav>
-      "
-      <div>
-        {["Legion", "Think Pad", "Yoga-Series", "Idea-Pad"].map((nav) => (
-          <div key={nav}>{nav}</div>
-        ))}
-      </div>
-      <div>
-        <img src={searchImg} alt="search" width={18} height={18} />
-        <img src={bagImg} alt="" width={18} height={18} />
-      </div>
     </header>
   );
 };
