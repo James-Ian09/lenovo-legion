5d9e82f1af54705f85faa3f6cf0ad2a32d83ec9a
James-Ian09 <jamesianbayonas21@gmail.com>
2025-05-10 20:57:26 +0800

    I have added something

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return <section className="w-full bg-black ">Hero</section>;
 };
 
-export default Hero;
+export default Hero; this is where I have left off
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,10 +4,9 @@ import { navLists } from "../constants";
 const Navbar = () => {
   // const is just a convention, you can use let or var as well
   return (
-    <header className="w-full py-5 sm:px-10 px-5 flex-justify-">
+    <header className="w-full py-5 sm:px-10 px-5 flex-justify-between items-center">
       <nav className="flex w-full screen-max-width">
         <img src={appleImg} alt="Apple" width={14} height={18} />
-
         <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((nav: string) => (
             <div
@@ -18,9 +17,12 @@ const Navbar = () => {
             </div>
           ))}
         </div>
-        <div>
+
+        <div className="flex item-baseline gap-7 max-sm:justify-end max-sm:flex-1 ">
+          {" "}
+          {/* t-k-wrd which is baseline is used to make the element appear in a horizontal form */}
           <img src={searchImg} alt="search" width={18} height={18} />
-          <img src={bagImg} alt="" width={18} height={18} />
+          <img src={bagImg} alt="bag" width={18} height={18} />
         </div>
       </nav>
     </header>
