a3c554f1cf14f9a4da76ec6a815579397cd9fb58
James-Ian09 <jamesianbayonas21@gmail.com>
2025-05-12 22:59:11 +0800

    Add some features to the website

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -55,6 +55,7 @@ const Hero = () => {
         </a>
         <p className="font-normal text-xl">Buy one take one</p>
       </div>
+      <p>This is a testing to see if it really works or not </p>
     </section>
   );
 };
