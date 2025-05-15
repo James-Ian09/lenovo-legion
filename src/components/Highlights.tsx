e854c44a4a3f7950f274ccbf9f022273c9e0d9f5
James-Ian09 <jamesianbayonas21@gmail.com>
2025-05-15 14:41:41 +0800

    I have added files

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -1,10 +1,11 @@
 import { useGSAP } from "@gsap/react";
-import { watchImg } from "../utils";
+import { watchImg, rightImg } from "../utils";
 import gsap from "gsap";
 
 const Highlights = () => {
   useGSAP(() => {
     gsap.to("#title", { opacity: 1, y: 0 });
+    gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.25 }); // stagger is used to create a delay between the animations of each element, via** .link class
   }, []);
   return (
     <section
@@ -12,14 +13,21 @@ const Highlights = () => {
       className="w-screen overflow-hidden h-full common-padding bg-zinc"
     >
       <div className="screen-max-width">
-        <div>
+        <div className="mb-12 w-full items-end justify-between md:flex">
           <h1 id="title" className="section-heading">
             Get the highlights.
           </h1>
 
           <div className="flex flex-wrap items-end gap-5">
-            <p className="link">Watch the film </p>
-            <img src={watchImg} alt="watch" className="ml-2" />
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="ml-2" />
+            </p>
+
+            <p className="link">
+              Don't miss out event's
+              <img src={rightImg} alt="right" className="ml-2" />
+            </p>
           </div>
         </div>
       </div>
