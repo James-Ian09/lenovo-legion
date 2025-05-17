87acc1c84c4673b158c6c35e25e7035b09941f99
James-Ian09 <jamesianbayonas21@gmail.com>
2025-05-17 15:11:21 +0800

    Added some styling

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -6,7 +6,7 @@ import VideoCarousel from "./VideoCarousel";
 const Highlights = () => {
   useGSAP(() => {
     gsap.to("#title", { opacity: 1, y: 0 });
-    gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.25 }); // stagger is used to create a delay between the animations of each element, via** .link class
+    gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.9 }); // stagger is used to create a delay between the animations of each element, via** .link class
   }, []);
   return (
     <section
--- src/components/VideoCarousel.tsx
+++ src/components/VideoCarousel.tsx
@@ -4,18 +4,24 @@ const VideoCarousel = () => {
   return (
     <>
       <div className="flex item-center">
-      {hightlightsSlides.map((list, i) => (
-        <div key = {list.id} id = "slider">
-          <div className = "video-carousel_container">
-            <div className = "w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
-              <video>
-                <source src={list.video} type="video/mp4" />
-              </video>
-
+        {hightlightsSlides.map((list, i) => (
+          <div key={list.id} id="slide ">
+            <div className="video-carousel_container">
+              <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+                <video id="video" playsInline={true} preload="auto" muted>
+                  <source src={list.video} type="video/mp4" />
+                </video>
+              </div>
+              <div className="absolute top-12 left-[5%] z-10">
+                {list.textLists.map((text, i) => (
+                  <p key={i} className="md:text-2xl text-xl font-medium">
+                    {text}
+                  </p>
+                ))}
+              </div>
             </div>
           </div>
-        <div/>
-      ))}
+        ))}
       </div>
     </>
   );
