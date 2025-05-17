83372d6255615a30ed3df1fe9237510d213a9306
James-Ian09 <jamesianbayonas21@gmail.com>
2025-05-17 23:52:48 +0800

    Added some content but alrighty

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./index.css"; // This is the file that needed a declaration module.
+
+//fix the index.css - this is the file that needed a declaration module.
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
