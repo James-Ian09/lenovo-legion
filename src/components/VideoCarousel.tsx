08a50f19e8161e9360ae34081c6e7c1d1f20f938
James-Ian09 <jamesianbayonas21@gmail.com>
2025-05-17 13:13:33 +0800

    fixing certain typo

--- src/components/VideoCarousel.tsx
+++ src/components/VideoCarousel.tsx
@@ -7,6 +7,12 @@ const VideoCarousel = () => {
       {hightlightsSlides.map((list, i) => (
         <div key = {list.id} id = "slider">
           <div className = "video-carousel_container">
+            <div className = "w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+              <video>
+                <source src={list.video} type="video/mp4" />
+              </video>
+
+            </div>
           </div>
         <div/>
       ))}
